df934b4e1d30c279b334e5fe8fd41e9feb9a313e
Teranikys <97176914+Teranikys@users.noreply.github.com>
2023-09-11 16:11:33 +0300

    added lab5

---
+++ src/ru/mirea/lab5/task4/Task4.java
@@ -0,0 +1,21 @@
+package ru.mirea.lab5.task4;
+
+import javax.swing.*;
+
+public class Task4 {
+    public static void main(String[] args) throws InterruptedException {
+        JFrame frame = new JFrame("GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(509, 604);
+        JLabel pic = new JLabel();
+        frame.add(pic);
+        frame.setVisible(true);
+
+        for (int i = 0; ; i = (i + 1) % 12) {
+            ImageIcon imageIcon = new ImageIcon("C:\\InteliJProjects\\MIREAProjects\\Lab1\\src\\ru\\mirea\\lab5\\task4\\resources\\" + (i + 1) + ".png");
+            pic.setIcon(imageIcon);
+            pic.repaint();
+            Thread.sleep(150);
+        }
+    }
+}
